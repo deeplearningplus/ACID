2585bb8cadbae9e8f84e93983d2011692359c4c8
deeplearningplus <121497429+deeplearningplus@users.noreply.github.com>
2024-01-03 15:47:53 +0800

    Update generate_random_weight.py

--- generate_random_weight.py
+++ generate_random_weight.py
@@ -1,4 +1,4 @@
 import transformers
 config = transformers.OPTConfig.from_json_file("config.json")
 model = transformers.OPTForCausalLM(config)
-model.save_pretrained("opt-seq-pubmed-125m")
+model.save_pretrained("opt-seq-pubmed-tokenizer")
